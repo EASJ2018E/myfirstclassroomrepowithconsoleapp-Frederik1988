ca1e73f9fa7896dad0ce8ff08ab50af6857cf907
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-05 13:16:28 +0200

    første opgave

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -10,8 +10,11 @@ namespace MyFirstGitHubClassRoomConsoleApp
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Så er den fixet");
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+            Console.ReadKey();
         }
     }
 }
